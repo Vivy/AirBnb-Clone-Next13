a99244c823fc2b4d4e41fef121575157c57559a4
Vivy <Tomitaviorel@yahoo.com>
2023-04-05 11:13:00 +0300

    feat: added tailwind for style

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import { Nunito } from 'next/font/google';
 
 import './globals.css';
+import Navbar from './component/navbar/navbar';
 
 export const metadata = {
   title: 'AirBnB clone',
@@ -11,14 +12,15 @@ const font = Nunito({
   subsets: ['latin'],
 });
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang='en'>
-      <body className={font.className}>{children}</body>
+      <body className={font.className}>
+        <Navbar />
+        {children}
+      </body>
     </html>
   );
-}
+};
+
+export default RootLayout;
