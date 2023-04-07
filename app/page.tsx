69b00e7d1fb78fe198871225c0d1809e37877362
Vivy <Tomitaviorel@yahoo.com>
2023-04-07 13:07:09 +0300

    feat: create empty state for filtering searches

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,37 @@
+import ClientOnly from './component/clientonly/clientonly';
+import Container from './component/container/container';
+import EmptyState from './component/emptystate/emptystate';
+
 const Home = () => {
-  return <div>Hello Air bnb</div>;
+  const isEmpty = true;
+  if (isEmpty) {
+    return (
+      <ClientOnly>
+        <EmptyState showReset />
+      </ClientOnly>
+    );
+  }
+  return (
+    <ClientOnly>
+      <Container>
+        <div
+          className='
+          pt-24
+          grid
+          grid-cols-1
+          sm:grid-cols-2
+          md:grid-cols-3
+          lg:grid-cols-4
+          xl:grid-cols-5
+          2xl:grid-cols-6
+          gap-8
+        '
+        >
+          <div>My future listings</div>
+        </div>
+      </Container>
+    </ClientOnly>
+  );
 };
 
 export default Home;
