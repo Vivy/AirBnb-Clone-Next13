834184200132e1e8bf413c55191d90c50656b2dc
Vivy <Tomitaviorel@yahoo.com>
2023-04-08 14:03:04 +0300

    feat: listing done, added listing category and map

--- app/component/listinginfo/listinginfo.tsx
+++ app/component/listinginfo/listinginfo.tsx
@@ -1,7 +1,15 @@
 'use client';
 
+import useCountries from '@/app/hooks/usecontries';
 import { SafeUser } from '@/app/types';
 import { IconType } from 'react-icons';
+import Avatar from '../avatar/avatar';
+import ListingCategory from '../listingcategory/listingcategory';
+import dynamic from 'next/dynamic';
+
+const Map = dynamic(() => import('../map/map'), {
+  ssr: false,
+});
 
 interface ListingInfoType {
   user: SafeUser;
@@ -23,8 +31,60 @@ const ListingInfo: React.FC<ListingInfoType> = ({
   category,
   description,
   user,
+  bathroomCount,
+  guestCount,
+  locationValue,
+  roomCount,
 }) => {
-  return <div>ListingInfo</div>;
+  const { getByValue } = useCountries();
+
+  const coordonates = getByValue(locationValue)?.latlng;
+
+  return (
+    <div className='col-span-4 flex flex-col gap-8'>
+      <div className='flex flex-col gap-2'>
+        <div
+          className='
+              text-xl
+              font-semibold
+              flex
+              flex-row
+              items-center
+              gap-2
+      '
+        >
+          <div>Hosted by {user?.name}</div>
+          <Avatar src={user?.image} />
+        </div>
+        <div
+          className='
+               flex 
+               flex-row 
+               items-center 
+               gap-4 
+               font-light
+               text-neutral-500
+        '
+        >
+          <div>{guestCount} guests</div>
+          <div>{roomCount} rooms</div>
+          <div>{bathroomCount} bathrooms</div>
+        </div>
+      </div>
+      <hr />
+      {category && (
+        <ListingCategory
+          icon={category.icon}
+          label={category.label}
+          description={category.description}
+        />
+      )}
+      <hr />
+      <div className='text-lg font-light text-neutral-500'>{description}</div>
+      <hr />
+      <Map center={coordonates} />
+    </div>
+  );
 };
 
 export default ListingInfo;
